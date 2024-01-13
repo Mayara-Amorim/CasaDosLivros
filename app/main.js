70265cb288aff61107dc4e876074a95b32b84c96
Mayara Amorim <mayara.moreira@fenixeditorial.com.br>
2024-01-12 21:18:58 -0300

    Trazendo info assincronas

---
+++ app/main.js
@@ -0,0 +1,31 @@
+const api = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+let livros= [];
+const elementoParaInserirLivros = document.getElementById("livros");
+getBuscarLivros();
+getBuscarLivros();
+async function getBuscarLivros() {
+    const res = await fetch(api)
+   livros = await res.json();
+   console.table(livros);
+   exibirLivros(livros);
+
+}
+function exibirLivros(listaDeLivros) {
+    listaDeLivros.forEach(livro =>{
+        elementoParaInserirLivros.innerHTML+=`
+         <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao"> ${livro.autor}</p>
+      <p class="livro__preco" id="preco"> ${livro.preco}</p>
+      <div class="tags">
+        <span class="tag"> ${livro.categoria}</span>
+      </div>
+    </div>
+        
+        `
+    })
+}
+
